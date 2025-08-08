e2297394e7b67ab82a239d7c7c011c6b12689c73
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-08 12:03:38 -0300

    atualizações

--- java-advanced/aula-01/src/main/java/br/com/fiap/aula_01/controller/ProjetoController.java
+++ java-advanced/aula-01/src/main/java/br/com/fiap/aula_01/controller/ProjetoController.java
@@ -25,6 +25,15 @@ public class ProjetoController {
 
         map.put(1, new Projeto(1, "PROJETO 1", "Mateus"));
         map.put(2, new Projeto(2, "PROJETO 2", "Miguel"));
+        map.put(3, new Projeto(3, "PROJETO 3", "Lucas"));
+        map.put(4, new Projeto(4, "PROJETO 4", "Laura"));
+        map.put(5, new Projeto(5, "PROJETO 5", "Ana"));
+        map.put(6, new Projeto(6, "PROJETO 6", "Pedro"));
+        map.put(7, new Projeto(7, "PROJETO 7", "Juliana"));
+        map.put(8, new Projeto(8, "PROJETO 8", "Rafael"));
+        map.put(9, new Projeto(9, "PROJETO 9", "Camila"));
+        map.put(10, new Projeto(10, "PROJETO 10", "Bruno"));
+
 
         return map;
     }
